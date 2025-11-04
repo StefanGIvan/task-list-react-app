2e0971ce0bb103daaaabdfc2449cebb8972e38e1
Stefan Gabriel Ivan <stefanivan41@gmail.com>
2025-11-04 12:04:25 +0200

    Added comments to Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
+// configureStore - create Redux store
+// combineReducers - merge multiple slice reducers into one root reducer
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
+// persistReducer - wraps reducer so state is auto-saved/auto-loaded
+// persistStore - create a persistor object that coordinates loading
 import { persistReducer, persistStore } from "redux-persist";
 
+// storage engine
 import storage from "redux-persist/lib/storage";
 
+// the slice reducer (rootReducer)
 import tasksReducer from "../features/tasks/tasksSlice";
 
 // Take all slices and combine them
@@ -12,13 +18,16 @@ const rootReducer = combineReducers({
 });
 
 const persistConfig = {
-  key: "root",
-  storage,
+  key: "root", //save under the key: root (in localStorage persist:root)
+  storage, //use localStorage
   whitelist: ["tasks"], //slice to persist
 };
 
+// On startup: rehydrate from storage into Redux
+// On updates: write new state to storage
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+// Build the store with the wrapped persistedReducer
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefault) =>
@@ -29,4 +38,5 @@ export const store = configureStore({
     }),
 });
 
+// Create the persistor controller that will be passed to <PersistGate> (main.jsx) - UI waits until rehydration is finished
 export const persistor = persistStore(store);
